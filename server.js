657e9a647741cff6d1b6e126d0500738c11cd1fd
truongvu2005 <vu67893@gmail.com>
2026-03-12 21:09:09 +0700

    Fix thứ tự khởi tạo app và dọn dẹp code

--- server.js
+++ server.js
@@ -444,7 +444,8 @@ app.post('/v1/auth/login', async (req, res) => {
       user: {
         id: user.id,
         email: user.email,
-        full_name: user.full_name
+        full_name: user.full_name,
+        role: user.role // <--- CHỈ CẦN THÊM ĐÚNG DÒNG NÀY
       }
     });
 
